1ca6c1aeeb72b194643b5e9b135570970feb49df
Anushwar Sharma <anushwar.sharma@niyotail.com>
2023-04-27 22:18:17 +0530

    feat(pwa): setup app as pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
 
 /** @type {import('next').NextConfig} */
+import withPWA from 'next-pwa';
+
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pwa = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+export default pwa(nextConfig);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import Board from '@/components/Board';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <main className="mx-auto my-5 max-w-lg p-2 ">
-      <Header />
-      <Board />
-      <Footer />
-    </main>
+    <>
+      <Head>
+        <title>Play 2048!</title>
+      </Head>
+      <main className="mx-auto my-5 max-w-lg p-2 ">
+        <Header />
+        <Board />
+        <Footer />
+      </main>
+    </>
   );
 }
