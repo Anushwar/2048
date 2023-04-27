eaa0ab3d746628a16cb77e7545ff8f261362700e
Anushwar Sharma <anushwar.sharma@niyotail.com>
2023-04-27 17:58:05 +0530

    style(header): update header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,13 +8,13 @@ const Header = () => {
   return (
     <>
       <div className="flex justify-between align-middle">
-        <h1 className="text-7xl font-bold">2048</h1>
+        <h1 className="text-5xl font-bold">2048</h1>
         <div className="flex gap-5">
-          <div className="m-auto rounded-md bg-[#bbada0] p-5 text-center font-bold">
+          <div className="m-auto rounded-md bg-[#bbada0] p-6 text-center font-bold">
             <div className="font-bold uppercase">Score</div>
             <div>{score}</div>
           </div>
-          <div className="m-auto rounded-md border-2 bg-[#bbada0] p-5 text-center font-bold">
+          <div className="m-auto rounded-md border-2 bg-[#bbada0] p-6 text-center font-bold">
             <div className="font-bold uppercase">Best</div>
             <div>{best}</div>
           </div>
