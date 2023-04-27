64964a01b3033e0811e669e9d51a009cbc73c1fe
Anushwar Sharma <anushwar.sharma@niyotail.com>
2023-04-27 12:50:20 +0530

    feat(control): add actions to control

--- src/components/Control.tsx
+++ src/components/Control.tsx
@@ -1,12 +1,18 @@
+import useAppDispatch from '@/hooks/useAppDispatch';
+import useAppSelector from '@/hooks/useAppSelector';
+import { resetAction } from '@/store/action';
+
 const Control = () => {
+  const dispatch = useAppDispatch();
+  const size = useAppSelector((state) => state.app.boardSize);
   return (
     <div className="my-2 flex w-full justify-between gap-5">
       <div className="flex flex-col gap-2">
         <p className="text-center font-bold">Board size</p>
         <div className="flex w-full flex-row justify-between gap-2">
-          <button>-</button>
-          <div>{100}</div>
-          <button>+</button>
+          <button onClick={() => dispatch(resetAction(size - 1))}>-</button>
+          <div>{size}</div>
+          <button onClick={() => dispatch(resetAction(size + 1))}>+</button>
         </div>
       </div>
       <button>New game</button>
